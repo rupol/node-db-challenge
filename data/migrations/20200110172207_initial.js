0641ff7705e57292df8d028c23ac6a4fdb55f7ee
Ruth Poliakon <r.poliakon@gmail.com>
2020-01-10 17:48:03 -0700

    initial migration

--- data/migrations/20200110172207_initial.js
+++ data/migrations/20200110172207_initial.js
@@ -1,8 +1,61 @@
+exports.up = async function(knex) {
+  await knex.schema.createTable("projects", table => {
+    table.increments("id");
+    table.string("name").notNullable();
+    table.string("description");
+    table
+      .boolean("is_completed")
+      .notNullable()
+      .defaultTo(false);
+  });
 
-exports.up = function(knex) {
-  
+  await knex.schema.createTable("resources", table => {
+    table.increments("id");
+    table
+      .string("name")
+      .notNullable()
+      .unique();
+    table.string("description");
+  });
+
+  await knex.schema.createTable("tasks", table => {
+    table.increments("id");
+    table.string("description").notNullable();
+    table.string("notes");
+    table
+      .boolean("is_completed")
+      .notNullable()
+      .defaultTo(false);
+    table
+      .integer("project_id")
+      .notNullable()
+      .references("id")
+      .inTable("projects")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
+
+  await knex.schema.createTable("projects_resources", table => {
+    table
+      .integer("project_id")
+      .notNullable()
+      .references("id")
+      .inTable("projects")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    table
+      .integer("recource_id")
+      .notNullable()
+      .references("id")
+      .inTable("resources")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("projects_resources");
+  await knex.schema.dropTableIfExists("tasks");
+  await knex.schema.dropTableIfExists("resources");
+  await knex.schema.dropTableIfExists("projects");
 };
